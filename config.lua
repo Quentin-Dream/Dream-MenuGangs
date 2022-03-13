fef7f2e829ce9a571c245af344789ddf6b32d4be
Quentin-Dream <101477460+Quentin-Dream@users.noreply.github.com>
2022-03-13 14:24:09 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config = {
+
+-- Couleur des separateur dans menu ligoter
+couleur = { -- Voici la liste des couleurs : | ~r~ = Red | ~b~ = Blue | ~g~ = Green | ~y~ = Yellow | ~p~ = Purple | ~c~ = Grey | ~m~ = Dark Grey | ~u~ = Black | ~o~ = Orange |
+   couleur1 = "~p~",
+   couleur2 = "~w~",
+},
+
+-- Job2 ( gangs, orga etc..) qui ont acces au menu
+job = { -- pour ajouter un job2 qui aura acces au menu, il vous suffi juste de rajouter ceci => | {job2 = "nom du job2"}, |
+   {job2 = "vagos"},
+   {job2 = "famillies"},
+   {job2 = "ballas"},
+   {job2 = "bikers"},
+   {job2 = "marabunta"},
+   {job2 = "mafia"},
+},
+}
